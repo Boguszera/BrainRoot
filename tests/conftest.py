bfad85bb2be3b790a48b015a0bd28dc067ec4aad
boguszera <bogusz.biznes@gmail.com>
2025-03-29 03:46:46 +0100

    PYTHONPATH fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,12 @@ import sys
 import os
 
 # dodanie katalogu 'app' do ścieżki importu
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+if 'app' not in sys.path:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
 
 import pytest
 from app import create_app
 from app.models import db, Word, Category
-
-print(f"Current sys.path: {sys.path}")
 
 @pytest.fixture
 def app():
